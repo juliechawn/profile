4abfa09a4b589c7ca886f1b47ea1bef7fedcca33
Julie C  Hawn <juliechawn@gmail.com>
2019-03-22 08:02:19 -0400

    more changes

--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,9 +1,6 @@
 import React from "react";
 import "./Weather.css";
 
-import CityEntries from "./components/CityEntries";
-import CityInfo from "./components/CityInfo";
-import SearchHistory from "./components/SearchHistory";
 import Background from "./components/Background";
 
 export default class Weather extends React.Component {
