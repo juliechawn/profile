5c33691bd42273e36e99423510e58beb51f572f0
Julie C  Hawn <juliechawn@gmail.com>
2019-05-01 15:26:49 -0400

    more changes

--- src/components/Got/aws-exports.js
+++ src/components/Got/aws-exports.js
@@ -1,8 +1,8 @@
 const awsmobile =  {
-    "aws_appsync_graphqlEndpoint": process.env.REACT_APP_AWS_WEB,
-    "aws_appsync_region": process.env.REACT_APP_REGION,
+    "aws_appsync_graphqlEndpoint": REACT_APP_AWS_WEB,
+    "aws_appsync_region": REACT_APP_REGION,
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": process.env.REACT_APP_AWS_KEY,
+    "aws_appsync_apiKey": REACT_APP_AWS_KEY,
 };
 
 export default awsmobile;
