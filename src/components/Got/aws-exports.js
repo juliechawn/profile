fd56c5fd9dde10135ede8f914ddca34782b82334
Julie C  Hawn <juliechawn@gmail.com>
2019-05-01 15:37:39 -0400

    more changes

--- src/components/Got/aws-exports.js
+++ src/components/Got/aws-exports.js
@@ -1,6 +1,6 @@
 const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://ymgqk7qr2jdjpbag2hx77woou4.appsync-api.us-east-2.amazonaws.com/graphql",
-    "aws_appsync_region": process.env.REACT_APP_REGION,
+    "aws_appsync_region": "us-east-2",
     "aws_appsync_authenticationType": "API_KEY",
     "aws_appsync_apiKey": "da2-kh2ukxuchzfj3aalaob7beeloi",
 };
